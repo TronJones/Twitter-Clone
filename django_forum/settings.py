5d5f1d37f9872bbf7879ac80ffef0df5392f29d4
TronJones <94083340+TronJones@users.noreply.github.com>
2022-02-02 17:17:07 -0500

    Update settings.py

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-b$@4h1__l6fsjd-l!oend03fhn^icc-wca9vp^cc&6q9z^95w+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
